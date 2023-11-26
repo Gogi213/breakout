02936aa82796dd83964e682bcd0eb1be7a084cb2
Redmi <1993georgiy@gmail.com>
2023-11-26 16:13:11 +0200

    123123

--- breakout.py
+++ breakout.py
@@ -7,7 +7,7 @@ def find_local_maxima(df, left_span=7, right_span=7):
             local_maxima.append(i)
     return df.iloc[local_maxima]
 
-def find_tests(df, local_maxima, threshold=0.0006):
+def find_tests(df, local_maxima, threshold=0.0006, neighbors=5):
     tests = []
     for index, max_row in local_maxima.iterrows():
         max_price = max_row['High']
@@ -15,10 +15,14 @@ def find_tests(df, local_maxima, threshold=0.0006):
         test_candidates = df[(df.index > index) & (df['High'] <= max_price) & (df['High'] >= lower_bound)]
         test_candidates = test_candidates[test_candidates.index > index + 7]
 
-        last_test_price = max_price
         for test_index, test_row in test_candidates.iterrows():
-            if test_row['High'] < last_test_price:
-                tests.append(test_index)
-                last_test_price = test_row['High']
+            # Проверка, что тест-бар выше своих соседей
+            if test_index >= neighbors and test_index < len(df) - neighbors:
+                left_neighbors = df['High'][test_index-neighbors:test_index]
+                right_neighbors = df['High'][test_index+1:test_index+neighbors+1]
+                if test_row['High'] > left_neighbors.max() and test_row['High'] > right_neighbors.max():
+                    tests.append(test_index)
 
     return df.loc[tests]
+
+
