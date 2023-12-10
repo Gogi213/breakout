8ec3b06852a3fe0e674d28d98822772df1000f25
Redmi <1993georgiy@gmail.com>
2023-12-11 01:27:12 +0200

    123123

--- breakout.py
+++ breakout.py
@@ -9,6 +9,7 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 class BreakoutFinder:
     def __init__(self, df, prd=5, prd2=3, bo_len=200, cwidthu=0.03):
+        logging.info("Инициализация BreakoutFinder")
         self.df = df
         self.prd = prd
         self.prd2 = prd2
@@ -22,20 +23,16 @@ class BreakoutFinder:
     def calculate_pivot_points(self):
         logging.info("Вычисление Pivot Points")
 
-        # Проверка на пустоту DataFrame
         if self.df.empty:
             logging.warning("DataFrame is empty")
             return
 
-        # Сброс индексов DataFrame
         self.df.reset_index(drop=True, inplace=True)
 
-        # Присваивание значений Pivot Points
         pivot_highs = pivothigh(self.df, left=self.prd, right=self.prd)
         pivot_lows = pivotlow(self.df, left=self.prd, right=self.prd)
         self.df = self.df.assign(PivotHigh=pivot_highs, PivotLow=pivot_lows)
 
-        # Хранение значений и местоположений Pivot Points
         for i in range(len(self.df)):
             if not pd.isna(self.df['PivotHigh'][i]):
                 self.phval.append(self.df['PivotHigh'][i])
@@ -43,20 +40,11 @@ class BreakoutFinder:
             if not pd.isna(self.df['PivotLow'][i]):
                 self.plval.append(self.df['PivotLow'][i])
                 self.plloc.append(i)
+        logging.info("Pivot Points вычислены")
 
     def calculate_breakout_width(self):
-        # Расчет ширины прорыва
+        logging.info("Расчет ширины прорыва")
         highest_high = self.df['High'].rolling(window=self.prd).max()
         lowest_low = self.df['Low'].rolling(window=self.prd).min()
         self.df.loc[:, 'BreakoutWidth'] = (highest_high - lowest_low) * self.cwidthu
-
-    def detect_breakouts(self):
-        # Вводная часть для обнаружения прорывов
-        bomax = np.nan  # Потенциальный уровень бычьего прорыва
-        bostart = -1  # Индекс начала потенциального бычьего прорыва
-        num = 0  # Количество баров в потенциальном диапазоне бычьего прорыва
-        hgst = self.df['High'].rolling(window=self.prd).max().shift(1)  # Наивысший максимум в периоде prd
-
-        bomin = np.nan  # Потенциальный уровень медвежьего прорыва
-        num1 = 0  # Количество баров в потенциальном диапазоне медвежьего прорыва
-        lwst = self.df['Low'].rolling(window=self.prd).min().shift(1)  # Самый низкий минимум в периоде prd
+        logging.info("Ширина прорыва рассчитана")
