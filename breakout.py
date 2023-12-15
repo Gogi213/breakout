4a82ad466055c6fb90de55a784054a175047062c
Redmi <1993georgiy@gmail.com>
2023-12-15 23:50:44 +0200

    123123

--- breakout.py
+++ breakout.py
@@ -7,7 +7,7 @@ from detect_breakouts import pivothigh, pivotlow, detect_breakouts
 # logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class BreakoutFinder:
-    def __init__(self, df, prd=7, prd2=4, bo_len=1500, cwidthu=0.055):
+    def __init__(self, df, prd=7, prd2=4, bo_len=1500, cwidthu=0.05):
         # logging.info("Инициализация BreakoutFinder")
         self.df = df
         self.prd = prd
@@ -20,37 +20,28 @@ class BreakoutFinder:
         self.plloc = []  # Массив для хранения местоположений Pivot Low
 
     def calculate_pivot_points(self):
-        # logging.info("Вычисление Pivot Points")
-
         if self.df.empty:
             logging.warning("DataFrame is empty")
             return
 
         self.df.reset_index(drop=True, inplace=True)
 
-        pivot_highs = pivothigh(self.df, left=self.prd, right=self.prd)
-        pivot_lows = pivotlow(self.df, left=self.prd, right=self.prd)
+        # Использование prd2, если уже найдены точки поворота
+        left_param = self.prd2 if self.phval or self.plval else self.prd
+        right_param = self.prd2 if self.phval or self.plval else self.prd
+
+        pivot_highs = pivothigh(self.df, left=left_param, right=right_param)
+        pivot_lows = pivotlow(self.df, left=left_param, right=right_param)
         self.df = self.df.assign(PivotHigh=pivot_highs, PivotLow=pivot_lows)
 
         for i in range(len(self.df)):
             if not pd.isna(self.df['PivotHigh'][i]):
                 self.phval.append(self.df['PivotHigh'][i])
                 self.phloc.append(i)
-                # logging.info(f"Pair: {self.df['Pair'][i]}, Pivot High: {self.df['PivotHigh'][i]} at index {i}")
 
             if not pd.isna(self.df['PivotLow'][i]):
                 self.plval.append(self.df['PivotLow'][i])
                 self.plloc.append(i)
-                # logging.info(f"Pair: {self.df['Pair'][i]}, Pivot Low: {self.df['PivotLow'][i]} at index {i}")
-
-        # logging.info("Pivot Points вычислены")
-
-    # def calculate_breakout_width(self):
-    #     # logging.info("Расчет ширины прорыва")
-    #     highest_high = self.df['High'].rolling(window=self.prd).max()
-    #     lowest_low = self.df['Low'].rolling(window=self.prd).min()
-    #     self.df.loc[:, 'BreakoutWidth'] = (highest_high - lowest_low) * self.cwidthu
-    #     # logging.info("Ширина прорыва рассчитана")
 
     def calculate_chwidth(self):
         highest_high = [None] * len(self.df)
@@ -65,3 +56,20 @@ class BreakoutFinder:
                               zip(highest_high, lowest_low)]
         print("chwidth calculated and added to DataFrame.")
         return self.df
+
+
+    def cleanup_old_pivot_points(self):
+        # Очистка старых точек Pivot High
+        self.phval, self.phloc = self._cleanup_pivot_points(self.phval, self.phloc)
+
+        # Очистка старых точек Pivot Low
+        self.plval, self.plloc = self._cleanup_pivot_points(self.plval, self.plloc)
+
+    def _cleanup_pivot_points(self, values, locations):
+        cleaned_values = []
+        cleaned_locations = []
+        for val, loc in zip(values, locations):
+            if len(self.df) - loc <= self.bo_len:
+                cleaned_values.append(val)
+                cleaned_locations.append(loc)
+        return cleaned_values, cleaned_locations
